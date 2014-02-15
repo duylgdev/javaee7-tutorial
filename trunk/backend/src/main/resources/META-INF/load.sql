458569d5b436a74c6cc305a76277d481c518e6f5
guihome.diaz@gmail.com <guihome.diaz@gmail.com@3ca5360a-df2c-8912-c502-e643559f9263>
2014-02-15 20:23:09 +0000

    Data set is complete and working

--- trunk/backend/src/main/resources/META-INF/load.sql
+++ trunk/backend/src/main/resources/META-INF/load.sql
@@ -1,109 +1,102 @@
 -- Administrator
-INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) 
-VALUES 
-	('1', '1', '2012-09-01 01:00:00','2013-02-14 01:00:00', 'Administrator', 'admin', 'admin@daxiongmao.eu', 'admin');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('1', '1', '2012-09-01 01:00:00','2013-02-14 01:00:00', 'Administrator', 'admin', 'admin@daxiongmao.eu', 'admin');
 
 -- standard users
-INSERT INTO `tutorial`.`users` (`active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) 
-VALUES 
-	('1', '1938-03-21 01:00:00','1938-03-21 01:00:00', 'Spirou', '', 'spirou', 'spirou@redaction.be', 'test'),
-	('1', '1984-01-01 01:00:00','1984-01-01 01:00:00', 'XII', '', 'xiii', 'xiii@comics.be', 'test'),
- 	('1', '1982-01-01 01:00:00','1982-01-01 01:00:00', 'Aria', '', 'aria', 'aria@comics.be', 'test'),
-	('1', '1994-01-01 01:00:00','1994-01-01 01:00:00', 'Lanfeust', 'de Troy', 'lanfeust', 'lanfeust@comics.fr', 'test'),
- 	('1', '1990-01-01 01:00:00','1990-01-01 01:00:00', 'Joe', 'Bar', 'joe', 'joe.bar@comics.be', 'test'),
-	('1', '1962-12-01 01:00:00','1962-12-01 01:00:00', 'Thor', 'Odinson', 'thor', 'thor@comics.us', 'test'),
-	('1', '1973-01-01 01:00:00','1973-01-01 01:00:00', 'Largo', 'Winch', 'largo', 'largo@comics.be', 'test'),
- 	('1', '1948-10-28 01:00:00','1948-10-28 01:00:00', 'Philip', 'Mortimer', 'mortimer', 'philip.mortimer@comics.be', 'test'),
- 	('1', '1957-02-28 01:00:00','1957-02-28 01:00:00', 'Gaston', 'Lagaffe', 'gaston', 'gaston@comics.be', 'test'),
-	('1', '1977-01-01 01:00:00','1977-01-01 01:00:00', 'Leonard', 'le Genie', 'leonard', 'leonard@comics.fr', 'test'),
-	('1', '1963-01-01 01:00:00','1963-01-01 01:00:00', 'Achile', 'talon', 'achile', 'achile@comics.be', 'test'),
-	('1', '1963-01-01 01:00:00','1963-01-01 01:00:00', 'Iron man', 'Tony Stark', 'iron-man', 'iron-man@comics.us', 'test'),
-	('1', '1988-01-01 01:00:00','1988-01-01 01:00:00', 'Agripinne', '', 'agripinne', 'agripinne@comics.fr', 'test'),
-	('1', '1959-10-29 01:00:00','1959-10-29 01:00:00', 'Astérix', 'Le gaulois', 'asterix', 'asterix@comics.fr', 'test'),
-	('1', '1929-01-01 01:00:00','1929-01-01 01:00:00', 'Tintin', '', 'tintin', 'tintin@comics.be', 'test'), 
-	('1', '1977-01-01 01:00:00','1977-01-01 01:00:00', 'Les bidochon', '', 'les-bidochon', 'les-bidochon@comics.fr', 'test'),
-	('1', '1948-01-01 01:00:00','1948-01-01 01:00:00', 'Alix', 'Graccus', 'alix', 'alix@comics.fr', 'test'),
-	('1', '1912-01-01 01:00:00','1912-01-01 01:00:00', 'Tarzan', '', 'tarzan', 'tarzan@comics.uk', 'test'),
-	('1', '2004-05-01 01:00:00','2004-05-01 01:00:00', 'Tamara', 'Boula', 'tamara', 'tamara@comics.be', 'test'),
-	('1', '1998-05-01 01:00:00','1998-05-01 01:00:00', 'Nävis', 'Sillage', 'navis', 'navis@comics.fr', 'test'),
-	('1', '1971-01-01 01:00:00','1971-01-01 01:00:00', 'Le concombre masqué', '', 'le-concombre', 'le-concombre@comics.fr', 'test'),
-	('1', '1977-01-01 01:00:00','1977-01-01 01:00:00', 'Les as', '', 'les-as', 'les-as@comics.ca', 'test'),
-	('1', '1932-01-01 01:00:00','1932-01-01 01:00:00', 'Superman', 'Clark Kent', 'superman', 'superman@comics.us', 'test'),	
-	('1', '1939-05-01 01:00:00','1939-05-01 01:00:00', 'Batman', 'Bruce Wayne', 'batman', 'batman@comics.us', 'test'),
-	('1', '1962-01-01 01:00:00','1962-01-01 01:00:00', 'Iznogoud', '', 'iznogoud', 'iznogoud@comics.fr', 'test'),
-	('1', '1905-01-01 01:00:00','1905-01-01 01:00:00', 'Bécassine', '', 'becassine', 'becassine@comics.fr', 'test'),
-	('1', '1954-01-01 01:00:00','1954-01-01 01:00:00', 'Pepito', '', 'pepito', 'pepito@comics.it', 'test'),
-	('1', '1964-01-01 01:00:00','1964-01-01 01:00:00', 'Mafalda', '', 'mafalda', 'mafalda@comics.ar', 'test'),
-	('1', '1947-12-01 01:00:00','1947-12-01 01:00:00', 'Picsou', '', 'picsou', 'picsou@comics.us', 'test'),
-	('1', '1983-01-01 01:00:00','1983-01-01 01:00:00', 'Le chat', '', 'le-chat', 'le-chat@comics.be', 'test'),
-	('1', '1958-01-01 01:00:00','1958-01-01 01:00:00', 'Les schtroumpfs', '', 'schtroumpfs', 'schtroumpfs@comics.be', 'test'),
-	('1', '1975-01-01 01:00:00','1975-01-01 01:00:00', 'Maya', "l\'abeille", 'maya', 'maya@comics.jp', 'test'),
-	('1', '1977-01-01 01:00:00','1977-01-01 01:00:00', 'Thorgal', '', 'thorgal', 'thorgal@comics.be', 'test'),
-	('1', '1950-10-01 01:00:00','1950-10-01 01:00:00', 'Snoopy', 'Peanuts', 'snoopy', 'snoopy@comics.us', 'test'),
-	('1', '1978-01-01 01:00:00','1978-01-01 01:00:00', 'Garfield', '', 'garfield', 'le-chat@comics.us', 'test'),
-	('1', '1970-01-01 01:00:00','1970-01-01 01:00:00', 'Yoko', 'Tsuno', 'yoko', 'yoko@comics.be', 'test'),
-	('1', '1976-01-01 01:00:00','1976-01-01 01:00:00', 'Adèle', 'Blanc-Sec', 'adele', 'adele@comics.fr', 'test'),	
-	('1', '1917-01-01 01:00:00','1917-01-01 01:00:00', 'Captain', 'America', 'captain', 'captain@comics.us', 'test'),
-	('1', '1961-01-01 01:00:00','1961-01-01 01:00:00', '4 Fantastiques', '', 'fantastiques', 'fantastiques@comics.us', 'test'),
-	('1', '1971-01-01 01:00:00','1971-01-01 01:00:00', 'Rahan', '', 'rahan', 'rahan@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('2', '1', '1994-01-01 01:00:00','1994-01-01 01:00:00', 'Lanfeust', 'de Troy', 'lanfeust', 'lanfeust@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('3', '1', '1990-01-01 01:00:00','1990-01-01 01:00:00', 'Joe', 'Bar', 'joe', 'joe.bar@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('4', '1', '1962-12-01 01:00:00','1962-12-01 01:00:00', 'Thor', 'Odinson', 'thor', 'thor@comics.us', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('5', '1', '1973-01-01 01:00:00','1973-01-01 01:00:00', 'Largo', 'Winch', 'largo', 'largo@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('6', '1', '1948-10-28 01:00:00','1948-10-28 01:00:00', 'Philip', 'Mortimer', 'mortimer', 'philip.mortimer@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('7', '1', '1957-02-28 01:00:00','1957-02-28 01:00:00', 'Gaston', 'Lagaffe', 'gaston', 'gaston@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('8', '1', '1977-01-01 01:00:00','1977-01-01 01:00:00', 'Leonard', 'le Genie', 'leonard', 'leonard@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('9', '1', '1963-01-01 01:00:00','1963-01-01 01:00:00', 'Achile', 'talon', 'achile', 'achile@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('10', '1', '1963-01-01 01:00:00','1963-01-01 01:00:00', 'Iron man', 'Tony Stark', 'iron-man', 'iron-man@comics.us', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('11', '1', '1948-01-01 01:00:00','1948-01-01 01:00:00', 'Alix', 'Graccus', 'alix', 'alix@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('12', '1', '1959-10-29 01:00:00','1959-10-29 01:00:00', 'Astérix', 'Le gaulois', 'asterix', 'asterix@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('13', '1', '2004-05-01 01:00:00','2004-05-01 01:00:00', 'Tamara', 'Boula', 'tamara', 'tamara@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('14', '1', '1998-05-01 01:00:00','1998-05-01 01:00:00', 'Nävis', 'Sillage', 'navis', 'navis@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('15', '1', '1932-01-01 01:00:00','1932-01-01 01:00:00', 'Superman', 'Clark Kent', 'superman', 'superman@comics.us', 'test');	
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('16', '1', '1939-05-01 01:00:00','1939-05-01 01:00:00', 'Batman', 'Bruce Wayne', 'batman', 'batman@comics.us', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('17', '1', '1975-01-01 01:00:00','1975-01-01 01:00:00', 'Maya', "l\'abeille", 'maya', 'maya@comics.jp', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('18', '1', '1950-10-01 01:00:00','1950-10-01 01:00:00', 'Snoopy', 'Peanuts', 'snoopy', 'snoopy@comics.us', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('19', '1', '1970-01-01 01:00:00','1970-01-01 01:00:00', 'Yoko', 'Tsuno', 'yoko', 'yoko@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('20', '1', '1976-01-01 01:00:00','1976-01-01 01:00:00', 'Adèle', 'Blanc-Sec', 'adele', 'adele@comics.fr', 'test');	
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `last_name`, `login`, `email`, `password`) VALUES	('21', '1', '1917-01-01 01:00:00','1917-01-01 01:00:00', 'Captain', 'America', 'captain', 'captain@comics.us', 'test');
+
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('22', '1', '1938-03-21 01:00:00','1938-03-21 01:00:00', 'Spirou', 'spirou', 'spirou@redaction.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('23', '1', '1984-01-01 01:00:00','1984-01-01 01:00:00', 'XII', 'xiii', 'xiii@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('24', '1', '1982-01-01 01:00:00','1982-01-01 01:00:00', 'Aria', 'aria', 'aria@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('25', '1', '1988-01-01 01:00:00','1988-01-01 01:00:00', 'Agripinne', 'agripinne', 'agripinne@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('26', '1', '1929-01-01 01:00:00','1929-01-01 01:00:00', 'Tintin', 'tintin', 'tintin@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('27', '1', '1977-01-01 01:00:00','1977-01-01 01:00:00', 'Les bidochon', 'les-bidochon', 'les-bidochon@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('28', '1', '1912-01-01 01:00:00','1912-01-01 01:00:00', 'Tarzan', 'tarzan', 'tarzan@comics.uk', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('29', '1', '1971-01-01 01:00:00','1971-01-01 01:00:00', 'Le concombre masqué', 'le-concombre', 'le-concombre@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('30', '1', '1977-01-01 01:00:00','1977-01-01 01:00:00', 'Les as', 'les-as', 'les-as@comics.ca', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('31', '1', '1962-01-01 01:00:00','1962-01-01 01:00:00', 'Iznogoud', 'iznogoud', 'iznogoud@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('32', '1', '1905-01-01 01:00:00','1905-01-01 01:00:00', 'Bécassine', 'becassine', 'becassine@comics.fr', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('33', '1', '1954-01-01 01:00:00','1954-01-01 01:00:00', 'Pepito', 'pepito', 'pepito@comics.it', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('34', '1', '1964-01-01 01:00:00','1964-01-01 01:00:00', 'Mafalda', 'mafalda', 'mafalda@comics.ar', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('35', '1', '1947-12-01 01:00:00','1947-12-01 01:00:00', 'Picsou', 'picsou', 'picsou@comics.us', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('36', '1', '1983-01-01 01:00:00','1983-01-01 01:00:00', 'Le chat', 'le-chat', 'le-chat@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('37', '1', '1958-01-01 01:00:00','1958-01-01 01:00:00', 'Les schtroumpfs', 'schtroumpfs', 'schtroumpfs@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('38', '1', '1977-01-01 01:00:00','1977-01-01 01:00:00', 'Thorgal', 'thorgal', 'thorgal@comics.be', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('39', '1', '1978-01-01 01:00:00','1978-01-01 01:00:00', 'Garfield', 'garfield', 'le-chat@comics.us', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('40', '1', '1961-01-01 01:00:00','1961-01-01 01:00:00', '4 Fantastiques', 'fantastiques', 'fantastiques@comics.us', 'test');
+INSERT INTO `tutorial`.`users` (`id`, `active`, `created`, `updated`, `first_name`, `login`, `email`, `password`) VALUES ('41', '1', '1971-01-01 01:00:00','1971-01-01 01:00:00', 'Rahan', 'rahan', 'rahan@comics.fr', 'test');
+	
+
 
 -- Group and related permission(s)
-INSERT INTO `tutorial`.`groups` (`id`, `active`, `created`, `updated`, `name`, `description`) 
-VALUES 
-	('1', '1', '2014-01-01 01:00:00','2014-01-01 01:00:00', 'Simple user', 'Application default user'),
-	('2', '1', '2014-01-01 01:00:00','2014-01-01 01:00:00', 'Administrator', "Application\'s administrator");
+INSERT INTO `tutorial`.`groups` (`id`, `active`, `created`, `updated`, `name`, `description`) VALUES ('1', '1', '2014-01-01 01:00:00','2014-01-01 01:00:00', 'Simple user', 'Application default user');
+INSERT INTO `tutorial`.`groups` (`id`, `active`, `created`, `updated`, `name`, `description`) VALUES ('2', '1', '2014-01-01 01:00:00','2014-01-01 01:00:00', 'Administrator', "Application\'s administrator");
 
 -- Set of rights
-INSERT INTO `tutorial`.`permissions` (`id`, `active`, `created`, `updated`, `name`, `description`) 
-VALUES 
-	('1', '1', '2014-01-01 01:00:00','2014-01-01 01:00:00', 'Administration', "Application\'s administration"),
-	('2', '1', '2014-01-01 01:00:00','2014-01-01 01:00:00', 'User profile', 'This allow the current user to change its profile settings: address, email, password, ... ');
+INSERT INTO `tutorial`.`permissions` (`id`, `active`, `created`, `updated`, `name`, `description`) VALUES ('1', '1', '2014-01-01 01:00:00','2014-01-01 01:00:00', 'Administration', "Application\'s administration");
+INSERT INTO `tutorial`.`permissions` (`id`, `active`, `created`, `updated`, `name`, `description`) VALUES ('2', '1', '2014-01-01 01:00:00','2014-01-01 01:00:00', 'User profile', 'This allow the current user to change its profile settings: address, email, password, ... ');
 	
 -- admin group
 INSERT INTO `tutorial`.`users_groups` () VALUES ('1', '2');
 
 -- all user belong to default group
-INSERT INTO `tutorial`.`users_groups` () 
-VALUES 
-	('1', '1'),
-	('2', '1'),
-	('3', '1'),
-	('4', '1'),
-	('5', '1'),
-	('6', '1'),
-	('7', '1'),
-	('8', '1'),
-	('9', '1'),
-	('10', '1'),
-	('11', '1'),
-	('12', '1'),
-	('13', '1'),
-	('14', '1'),
-	('15', '1'),
-	('16', '1'),
-	('17', '1'),
-	('18', '1'),
-	('19', '1'),
-	('20', '1'),
-	('21', '1'),
-	('22', '1'),
-	('23', '1'),
-	('24', '1'),
-	('25', '1'),
-	('26', '1'),
-	('27', '1'),
-	('28', '1'),
-	('29', '1'),
-	('30', '1'),
-	('31', '1'),
-	('32', '1'),
-	('33', '1'),
-	('34', '1'),
-	('35', '1'),
-	('36', '1'),
-	('37', '1'),
-	('38', '1'),
-	('39', '1'),
-	('40', '1'),
-	('41', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('1', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('2', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('3', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('4', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('5', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('6', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('7', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('8', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('9', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('10', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('11', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('12', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('13', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('14', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('15', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('16', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('17', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('18', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('19', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('20', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('21', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('22', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('23', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('24', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('25', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('26', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('27', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('28', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('29', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('30', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('31', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('32', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('33', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('34', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('35', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('36', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('37', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('38', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('39', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('40', '1');
+INSERT INTO `tutorial`.`users_groups` () VALUES ('41', '1');
 	
